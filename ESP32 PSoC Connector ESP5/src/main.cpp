dd8a68b35feb913a4fa811f24e3a7e765454ed6d
ILLVAN <silvanweber@gmail.com>
2023-06-05 16:22:24 +0200

    commented ESP32 code

--- ESP32 PSoC Connector ESP5/src/main.cpp
+++ ESP32 PSoC Connector ESP5/src/main.cpp
@@ -13,9 +13,10 @@
 #define RXD2 16
 #define TXD2 17
 #define OErestart 19
-HardwareSerial psocSerial(2); 
 
-U8X8_SH1106_128X64_NONAME_HW_I2C u8x8(SCL, SDA, U8X8_PIN_NONE);
+HardwareSerial psocSerial(2);                                       // Sserial used fpr PSoC communication
+
+U8X8_SH1106_128X64_NONAME_HW_I2C u8x8(SCL, SDA, U8X8_PIN_NONE);     // display constructor
 
 //''''''''''NOTES AND SCALES '''''''''''''''''''''''
 String notes[108]= {"C0 ", "C#0", "D0 ", "D#0", "E0 ", "F0 ", "F#0", "G0 ", "G#0", "A0 ", "A#0", "B0 ", "C1 ", "C#1", "D1 ", "D#1", "E1 ", "F1 ", "F#1", "G1 ", "G#1", "A1 ", "A#1", "B1 ", "C2 ", "C#2", "D2 ", "D#2", "E2 ", "F2 ", "F#2", "G2 ", "G#2", "A2 ", "A#2", "B2 ", "C3 ", "C#3", "D3 ", "D#3", "E3 ", "F3 ", "F#3", "G3 ", "G#3", "A3 ", "A#3", "B3 ", "C4 ", "C#4", "D4 ", "D#4", "E4 ", "F4 ", "F#4", "G4 ", "G#4", "A4 ", "A#4", "B4 ", "C5 ", "C#5", "D5 ", "D#5", "E5 ", "F5 ", "F#5", "G5 ", "G#5", "A5 ", "A#5", "B5 ", "C6 ", "C#6", "D6 ", "D#6", "E6 ", "F6 ", "F#6", "G6 ", "G#6", "A6 ", "A#6", "B6 ", "C7 ", "C#7", "D7 ", "D#7", "E7 ", "F7 ", "F#7", "G7 ", "G#7", "A7 ", "A#7", "B7 ", "C8 ", "C#8", "D8 ", "D#8", "E8 ", "F8 ", "F#8", "G8 ", "G#8", "A8 ", "A#8", "B8 "};
@@ -77,7 +78,7 @@ uint8_t cScales[6][8][8] =  {{      // WORLD PENTATONICS
     }
 };   
 
-const String cScaleGroupNames[6] = {"WORLD PENTA" , "BLUES & MINs", "DOMINANT-7", "CHURCH MODES", "HARMONIC MIN", "MELODIC MIN"};
+const String cScaleGroupNames[6] = {"WORLD PENTA" , "BLUES & MINs", "DOMINANT-7", "CHURCH MODES", "HARMONIC MIN", "MELODIC MIN"};  // display strings
 const String cScaleNames[6][8] = {
     // WORLD PENTATONICS
     {
@@ -146,7 +147,7 @@ const String cScaleNames[6][8] = {
         "7 altered/superlocrian [MM7]"
     }
 };
-const String cScaleShorts[6][8] = {
+const String cScaleShorts[6][8] = {   // short names to fit on screen
     // WORLD PENTATONICS
     {
         "MAJ penta",
@@ -218,86 +219,86 @@ const String cScaleShorts[6][8] = {
 
 
 
-hw_timer_t *displayTimer = NULL;
-const int capacity = 180;         // ca JSON_OBJECT_SIZE(xmembers) + x*JSON_OBJECT_SIZE(1member);
-StaticJsonDocument<capacity> JSONrec;
+hw_timer_t *displayTimer = NULL;        // timer for updating the display
+const int capacity = 180;               // ca JSON_OBJECT_SIZE(xmembers) + x*JSON_OBJECT_SIZE(1member); (JSON discouninued at the moment)
+StaticJsonDocument<capacity> JSONrec;   // JSON buffer
 
-volatile boolean dataToDisplay = 1;
-volatile double freq;
-volatile uint16_t intFreq;
-volatile String note;
-volatile uint8_t noteIndex;
-volatile uint8_t mode;         
-char displayRoot[5] = "n#Y";  
-char displayNote[5] = "n#Y";
-char displayGroupName[40] = "helloGroup";
-char displayScaleName[40] = "helloScale";
-uint8_t recRootDisplay;
-uint8_t recNoteDisplay;
-int8_t recOffsetDisplay;
-uint8_t recGroupDisplay;
-uint8_t recScaleDisplay;
-uint8_t recRootDisplayOld;
-uint8_t recNoteDisplayOld;
-int8_t recOffsetDisplayOld;
-uint8_t recGroupDisplayOld;
-uint8_t recScaleDisplayOld;
+volatile boolean dataToDisplay = 1; // Flag to indicate whether there is data to be displayed
+volatile double freq; // Frequency variable
+volatile uint16_t intFreq; // Integer frequency variable
+volatile String note; // Note variable
+volatile uint8_t noteIndex; // Index of the note
+volatile uint8_t mode; // Mode variable
+
+char displayRoot[5] = "n#Y"; // Character array for displaying root note
+char displayNote[5] = "n#Y"; // Character array for displaying note
+char displayGroupName[40] = "helloGroup"; // Character array for displaying group name
+char displayScaleName[40] = "helloScale"; // Character array for displaying scale name
+
+uint8_t recRootDisplay; // Display variable for received root note
+uint8_t recNoteDisplay; // Display variable for received note
+int8_t recOffsetDisplay; // Display variable for received offset
+uint8_t recGroupDisplay; // Display variable for received group
+uint8_t recScaleDisplay; // Display variable for received scale
+
+uint8_t recRootDisplayOld; // Old display variable for received root note
+uint8_t recNoteDisplayOld; // Old display variable for received note
+int8_t recOffsetDisplayOld; // Old display variable for received offset
+uint8_t recGroupDisplayOld; // Old display variable for received group
+uint8_t recScaleDisplayOld; // Old display variable for received scale
 
 
-volatile uint8_t cScaleGroupSelect = 100;                                 // 0 world penta | 1 blues and minors | 2 dominant scales | 3 church modes | 4 HM | 5 MM | 100 chromatic
-const uint8_t cScaleGroupsLength = 6;                                     // scale groups count
-volatile uint8_t cScaleSelect = 100; 
-volatile uint8_t displayTimerFlag = 0;
+volatile uint8_t cScaleGroupSelect = 100; // 0 world penta | 1 blues and minors | 2 dominant scales | 3 church modes | 4 HM | 5 MM | 100 chromatic
+const uint8_t cScaleGroupsLength = 6;   // scale groups count
+volatile uint8_t cScaleSelect = 100; // variable for selecting the scale
+volatile uint8_t displayTimerFlag = 0; // Flag indicating if display should be updated
 
 void IRAM_ATTR onDisplayTimer(){ 
-  displayTimerFlag = 1;
+  displayTimerFlag = 1; // Timer setting displayFlag
 }
 
-
 void setup() {
-  pinMode(OErestart, OUTPUT);
-  digitalWrite(OErestart, HIGH);
-  Serial.begin(115200);
-  Serial.println("USB SERIAL RUNNING");
-  psocSerial.begin(115200, SERIAL_8N1, RXD2, TXD2); 
-  Serial.println("PSOC SERIAL RUNNING");
-  u8x8.begin();
-  u8x8.setPowerSave(0);
-  u8x8.setFont(u8x8_font_chroma48medium8_r);
-  u8x8.setCursor(0,0);
-  displayTimer = timerBegin(0, 80, true); 
-  timerAttachInterrupt(displayTimer , &onDisplayTimer, true);
-  timerAlarmWrite(displayTimer, 1000, true); 
-  timerAlarmEnable(displayTimer);
-  digitalWrite(OErestart, HIGH);
-  
+  pinMode(OErestart, OUTPUT); // Set OErestart pin as OUTPUT
+  digitalWrite(OErestart, HIGH); // Set OErestart pin to HIGH
+  Serial.begin(115200); // Initialize serial communication with baud rate 115200
+  Serial.println("USB SERIAL RUNNING"); // Print message to indicate USB serial is running
+  psocSerial.begin(115200, SERIAL_8N1, RXD2, TXD2); // Initialize PSOC serial communication with baud rate 115200 and specific pins
+  Serial.println("PSOC SERIAL RUNNING"); // Print message to indicate PSOC serial is running
+  u8x8.begin(); // Initialize u8x8 display
+  u8x8.setPowerSave(0); // Disable power-saving mode of the display
+  u8x8.setFont(u8x8_font_chroma48medium8_r); // Set the font for the display
+  u8x8.setCursor(0,0); // Set the cursor position on the display
+  displayTimer = timerBegin(0, 80, true); // Initialize and configure a timer
+  timerAttachInterrupt(displayTimer , &onDisplayTimer, true); // Attach an interrupt function to the timer
+  timerAlarmWrite(displayTimer, 1000, true); // Set the timer alarm to trigger every 1000 microseconds
+  timerAlarmEnable(displayTimer); // Enable the timer alarm
+  digitalWrite(OErestart, HIGH); // Set OErestart pin to HIGH to anable voltage shifting
 }
 
 void loop() {
-  while (psocSerial.available()){
-    String serialReceive = psocSerial.readStringUntil('\r');   
-    // Serial.println(serialReceive);
-    uint8_t recStartIdx = (serialReceive.indexOf('|'));
-    serialReceive.remove(0, recStartIdx + 1);
-    uint8_t recEndIdx = (serialReceive.lastIndexOf('|'));
-    serialReceive.remove(recEndIdx);
-    // Serial.println(serialReceive);
+  while (psocSerial.available()) {
+    String serialReceive = psocSerial.readStringUntil('\r'); // Read serial data until a carriage return is encountered
+    uint8_t recStartIdx = (serialReceive.indexOf('|')); // Find the index of the first occurrence of '|'
+    serialReceive.remove(0, recStartIdx + 1); // Remove the characters before and including the '|'
+    uint8_t recEndIdx = (serialReceive.lastIndexOf('|')); // Find the index of the last occurrence of '|'
+    serialReceive.remove(recEndIdx); // Remove the characters after and including the '|'
 
-  int tempIntArray[5], r=0, t=0;
-  for (int i=0; i < serialReceive.length(); i++){ 
-    if(serialReceive.charAt(i) == ',') { 
-      tempIntArray[t] = serialReceive.substring(r, i).toInt(); 
-      r=(i+1); 
-      t++; 
+    int tempIntArray[5], r = 0, t = 0;
+    for (int i=0; i < serialReceive.length(); i++) {
+      if(serialReceive.charAt(i) == ',') { // Check if the character is a comma
+        tempIntArray[t] = serialReceive.substring(r, i).toInt(); // Convert the substring to an integer and store it in the temporary array
+        r = (i + 1); // Update the starting index for the next substring
+        t++; // Increment the temporary array index
+      }
     }
-  }
-  recRootDisplay = tempIntArray[0];
-  recNoteDisplay = tempIntArray[1];
-  recOffsetDisplay = tempIntArray[2];
-  recGroupDisplay = tempIntArray[3];
-  recScaleDisplay = tempIntArray[4];
+    recRootDisplay = tempIntArray[0]; // Assign the received root display value from the temporary array
+    recNoteDisplay = tempIntArray[1]; // Assign the received note display value from the temporary array
+    recOffsetDisplay = tempIntArray[2]; // Assign the received offset display value from the temporary array
+    recGroupDisplay = tempIntArray[3]; // Assign the received group display value from the temporary array
+    recScaleDisplay = tempIntArray[4]; // Assign the received scale display value from the temporary array
 
-// old JSON VERSION.. slow, too many bytes - but more comfortable
+
+    // old JSON VERSION.. discontinued: slow, too many bytes - but more comfortable
     // DeserializationError err = deserializeJson(JSONrec, serialReceive);
     // if (err) {
     //   Serial.println(err.f_str());
@@ -313,66 +314,65 @@ void loop() {
     // }
     // -------------------
   }
-  if(displayTimerFlag){
+  if (displayTimerFlag) {
     // OPTIONAL DEBUG PRINTS USB UART
-    Serial.println("Root " + (String) recRootDisplay);
-    Serial.println("Note " + (String)  recNoteDisplay);
-    Serial.println("Off2 " + (String)  recOffsetDisplay);
-    Serial.println("Group " + (String) recGroupDisplay);
-    Serial.println("Scale " + (String)  recScaleDisplay); 
-    if (recRootDisplay != recRootDisplayOld){
-      u8x8.clearLine(1);
-      u8x8.setCursor(0,1);
-      u8x8.print("root1: ");
-      u8x8.print(notes[recRootDisplay]); 
-    }
-    if (recRootDisplay != recRootDisplayOld || recOffsetDisplay != recOffsetDisplayOld){
-          u8x8.clearLine(2);
-      u8x8.setCursor(0,2);
-      u8x8.print("root2: ");
-      u8x8.print(notes[recRootDisplay + recOffsetDisplay + 16]);
-      if (recOffsetDisplay + 16 >= 0){
-        u8x8.print("+");
-      }
-      u8x8.print((String) (recOffsetDisplay + 16));
-    }
-    if (recNoteDisplay != recNoteDisplayOld){
-      u8x8.clearLine(3);
-      u8x8.setCursor(0,3);
-      u8x8.print("note1: ");
-      u8x8.print(notes[recNoteDisplay]);
-    }
-    if (recNoteDisplay != recNoteDisplayOld || recOffsetDisplay != recOffsetDisplayOld){
-      u8x8.clearLine(4);
-      u8x8.setCursor(0,4);
-      u8x8.print("note2: ");
-      u8x8.print(notes[recNoteDisplay + recOffsetDisplay + 16]);
-    }
-    
-    if (recGroupDisplay != recGroupDisplayOld || recScaleDisplay != recScaleDisplayOld){
-      u8x8.clearLine(5);
-      u8x8.clearLine(6);
-      u8x8.setCursor(0,5);
-      if (recGroupDisplay == 100){
-        u8x8.print(chromatic);
-      }
-      else{
-        u8x8.print(cScaleGroupNames[recGroupDisplay]);
-        u8x8.setCursor(0,6);
-        u8x8.print(cScaleShorts[recGroupDisplay][recScaleDisplay]);
-      }
-    }
-    u8x8.clearLine(0);
-    u8x8.clearLine(7);
-    displayTimerFlag = 0;
-    Serial.println("...");
-    // delay(10);
+    Serial.println("Root " + (String) recRootDisplay); // Print the received root display value
+    Serial.println("Note " + (String) recNoteDisplay); // Print the received note display value
+    Serial.println("Off2 " + (String) recOffsetDisplay); // Print the received offset display value
+    Serial.println("Group " + (String) recGroupDisplay); // Print the received group display value
+    Serial.println("Scale " + (String) recScaleDisplay); // Print the received scale display value
 
-    recRootDisplayOld = recRootDisplay;
-    recNoteDisplayOld = recNoteDisplay;
-    recOffsetDisplayOld = recOffsetDisplay;
-    recGroupDisplayOld = recGroupDisplay;
-    recScaleDisplayOld = recScaleDisplay;
-    
+    if (recRootDisplay != recRootDisplayOld) {
+      u8x8.clearLine(1); // Clear line 1 of the display
+      u8x8.setCursor(0,1); // Set the cursor to line 1
+      u8x8.print("root1: "); // Print the label for root1
+      u8x8.print(notes[recRootDisplay]); // Print the corresponding note for root1
+    }
+    if (recRootDisplay != recRootDisplayOld || recOffsetDisplay != recOffsetDisplayOld) {
+      u8x8.clearLine(2); // Clear line 2 of the display
+      u8x8.setCursor(0,2); // Set the cursor to line 2
+      u8x8.print("root2: "); // Print the label for root2
+      u8x8.print(notes[recRootDisplay + recOffsetDisplay + 16]); // Print the corresponding note for root2
+      if (recOffsetDisplay + 16 >= 0) {
+        u8x8.print("+"); // Print a "+" sign if the offset is positive or zero
+      }
+      u8x8.print((String) (recOffsetDisplay + 16)); // Print the offset value
+    }
+    if (recNoteDisplay != recNoteDisplayOld) {
+      u8x8.clearLine(3); // Clear line 3 of the display
+      u8x8.setCursor(0,3); // Set the cursor to line 3
+      u8x8.print("note1: "); // Print the label for note1
+      u8x8.print(notes[recNoteDisplay]); // Print the corresponding note for note1
+    }
+    if (recNoteDisplay != recNoteDisplayOld || recOffsetDisplay != recOffsetDisplayOld) {
+      u8x8.clearLine(4); // Clear line 4 of the display
+      u8x8.setCursor(0,4); // Set the cursor to line 4
+      u8x8.print("note2: "); // Print the label for note2
+      u8x8.print(notes[recNoteDisplay + recOffsetDisplay + 16]); // Print the corresponding note for note2
+    }
+
+    if (recGroupDisplay != recGroupDisplayOld || recScaleDisplay != recScaleDisplayOld) {
+      u8x8.clearLine(5); // Clear line 5 of the display
+      u8x8.clearLine(6); // Clear line 6 of the display
+      u8x8.setCursor(0,5); // Set the cursor to line 5
+      if (recGroupDisplay == 100) {
+        u8x8.print(chromatic); // Print the chromatic scale if the group is 100
+      }
+      else {
+        u8x8.print(cScaleGroupNames[recGroupDisplay]); // Print the name of the scale group
+        u8x8.setCursor(0,6); // Set the cursor to line 6
+        u8x8.print(cScaleShorts[recGroupDisplay][recScaleDisplay]); // Print the short name of the scale within the group
+      }
+    }
+    u8x8.clearLine(0); // Clear line 0 of the display
+    u8x8.clearLine(7); // Clear line 7 of the display
+    displayTimerFlag = 0; // Reset the display timer flag
+    Serial.println("..."); // Print ellipsis to indicate end of display update
+
+    recRootDisplayOld = recRootDisplay; // Store the current root display value for comparison in the next iteration
+    recNoteDisplayOld = recNoteDisplay; // Store the current note display value for comparison in the next iteration
+    recOffsetDisplayOld = recOffsetDisplay; // Store the current offset display value for comparison in the next iteration
+    recGroupDisplayOld = recGroupDisplay; // Store the current group display value for comparison in the next iteration
+    recScaleDisplayOld = recScaleDisplay; // Store the current scale display value for comparison in the next iteration
   }
 }
